303ea3837f0f5a6832fb20b9ef0f3b4e4ae1f833
Pigeon <kara.graysen@gmail.com>
2024-02-23 23:06:26 +0000

    Add for-loop to reduce data set fields

--- back-end/main.go
+++ back-end/main.go
@@ -3,8 +3,7 @@ package main
 import (
     "fmt"
     "log"
-	"os"
-	"io/ioutil"
+	"encoding/json"
     "net/http"
 )
 
@@ -17,7 +16,7 @@ func main() {
 
 // Root handler to direct usage.
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Golang! Use the /rss endpoint.")
+    fmt.Fprintf(w, "Hello from Golang! Use the /data endpoint.")
 }
 
 // App Store specific app ID this service is observing.
@@ -27,7 +26,6 @@ const appId = "595068606"
 func dataHandler(w http.ResponseWriter, r *http.Request) {
 	// Add appId to request URL.
     requestUrl := fmt.Sprintf("https://itunes.apple.com/us/rss/customerreviews/id=%s/sortBy=mostRecent/page=1/json", appId)
-	
 	request, err := http.NewRequest(http.MethodGet, requestUrl, nil)
 	
 	// Configure ResponseWriter to gracefully exit. 
@@ -50,17 +48,115 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Printf("Info: Client returned status code: %s with response", response.StatusCode)
+	defer response.Body.Close()
 
-	// Unwrap the response body.
-	responseBody, err := ioutil.ReadAll(response.Body)
+	// Marshal data into inbound data type.
+	var feedContainer InboundRSSFeedContainer
+    err = json.NewDecoder(response.Body).Decode(&feedContainer)
+    if err != nil {
+        panic(err)
+    }
+    
+	// Convert inbound data type to selective values for outbound data type.
+	var outboundContainer []OutboundContainerEntry
+	for _, element := range feedContainer.Feed.Entry { 
+		outboundContainer = append(outboundContainer, OutboundContainerEntry{Content: element.Content.Label, Author: element.Author.Name.Label, Score: element.ImRating.Label, Timestamp: element.Updated.Label})
+    } 
 
-	// Handle generalized error type for response body object.
-	// TODO: This could be expanded to handle specific scenarios.
-	if err != nil {
-		fmt.Printf("Error: Client was unable to read the response body: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-    	w.Write([]byte("500 - Uncaught exception while reading the response body"))
-	}
+	fmt.Printf("Info: Outbound container: %s", outboundContainer)
+}
 
-	fmt.Printf("Info: Client has response body: %s", responseBody)
+// Go type for data set stored in local disk file.s
+type OutboundContainerEntry struct {
+	Content   string `json:"content"`
+	Author    string `json:"author"`
+	Score     string `json:"score"`
+	Timestamp string `json:"timestamp"`
+}
+
+// Auto generated Go type from sampled JSON using
+// https://transform.tools/json-to-go
+type InboundRSSFeedContainer struct {
+	Feed struct {
+		Author struct {
+			Name struct {
+				Label string `json:"label"`
+			} `json:"name"`
+			URI struct {
+				Label string `json:"label"`
+			} `json:"uri"`
+		} `json:"author"`
+		Entry []struct {
+			Author struct {
+				URI struct {
+					Label string `json:"label"`
+				} `json:"uri"`
+				Name struct {
+					Label string `json:"label"`
+				} `json:"name"`
+				Label string `json:"label"`
+			} `json:"author"`
+			Updated struct {
+				Label string `json:"label"`
+			} `json:"updated"`
+			ImRating struct {
+				Label string `json:"label"`
+			} `json:"im:rating"`
+			ImVersion struct {
+				Label string `json:"label"`
+			} `json:"im:version"`
+			ID struct {
+				Label string `json:"label"`
+			} `json:"id"`
+			Title struct {
+				Label string `json:"label"`
+			} `json:"title"`
+			Content struct {
+				Label      string `json:"label"`
+				Attributes struct {
+					Type string `json:"type"`
+				} `json:"attributes"`
+			} `json:"content"`
+			Link struct {
+				Attributes struct {
+					Rel  string `json:"rel"`
+					Href string `json:"href"`
+				} `json:"attributes"`
+			} `json:"link"`
+			ImVoteSum struct {
+				Label string `json:"label"`
+			} `json:"im:voteSum"`
+			ImContentType struct {
+				Attributes struct {
+					Term  string `json:"term"`
+					Label string `json:"label"`
+				} `json:"attributes"`
+			} `json:"im:contentType"`
+			ImVoteCount struct {
+				Label string `json:"label"`
+			} `json:"im:voteCount"`
+		} `json:"entry"`
+		Updated struct {
+			Label string `json:"label"`
+		} `json:"updated"`
+		Rights struct {
+			Label string `json:"label"`
+		} `json:"rights"`
+		Title struct {
+			Label string `json:"label"`
+		} `json:"title"`
+		Icon struct {
+			Label string `json:"label"`
+		} `json:"icon"`
+		Link []struct {
+			Attributes struct {
+				Rel  string `json:"rel"`
+				Type string `json:"type"`
+				Href string `json:"href"`
+			} `json:"attributes"`
+		} `json:"link"`
+		ID struct {
+			Label string `json:"label"`
+		} `json:"id"`
+	} `json:"feed"`
 }
